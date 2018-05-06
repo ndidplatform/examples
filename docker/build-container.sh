98b2e1e48d4798de42b025f87535ec46c098e683
Parin Chiamananthapong <apemon642002@hotmail.com>
2018-05-06 23:53:31 +0700

    update docker compose

---
+++ docker/build-container.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+git clone https://github.com/ndidplatform/smart-contract.git &&
+git clone https://github.com/ndidplatform/api.git &&
+git clone https://github.com/ndidplatform/examples.git &&
+cd smart-contract &&
+git checkout add-docker &&
+docker build -t ndidplatform/tendermint:0.16.0 -f Dockerfile-tendermint . &&
+docker build -t ndidplatform/smart-contract -f Dockerfile-abci . &&
+cp -R docker-config ../docker-config &&
+cd ../api &&
+git checkout add-docker &&
+docker build -t ndidplatform/api -f Dockerfile . &&
+cd ../examples &&
+git checkout add-docker && cp docker\* . &&
+docker build -t ndidplatform/idp-examples -f Dockerfile-idp . &&
+docker build -t ndidplatform/rp-examples -f Dockerfile-rp . &&
+cd .. &&
+rm -r smart-contract &&
+rm -r api &&
+rm -r examples \
