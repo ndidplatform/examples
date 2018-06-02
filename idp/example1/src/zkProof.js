b51ff5a469614305cb600f77966a09daeb11f87c
0x0d01 <nutthadol@hotmail.com>
2018-06-02 12:50:21 +0700

    [idp] generate privatekey for zk using openssl instead of ssh-keygen

--- idp/example1/src/zkProof.js
+++ idp/example1/src/zkProof.js
@@ -73,7 +73,7 @@ export function calculateSecret(namespace, identifier,privateKey) {
 
 export function genNewKeyPair(sid) {
   let pathSid = './dev_user_key/' + sid;
-  let gen = spawnSync('ssh-keygen', ['-t', 'rsa', '-N', '', '-f', pathSid, '-b', '2048']);
+  let gen = spawnSync('openssl', ['genrsa', '-out', pathSid, '2048']);
   //console.log(gen.stderr.toString());
   let encode = spawnSync('openssl', ['rsa', '-in', pathSid, '-pubout', '-out', pathSid + '.pub']);
   //console.log(encode.stderr.toString());
